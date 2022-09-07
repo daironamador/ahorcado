985c856dfca3dd4dae957dcc65af186c467c1ab0
Dairon Amador <daironamador016@gmail.com>
2022-09-07 15:32:18 -0400

    Simplificando archivos y recursos html y css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
-'use strict'
+
 const host = 'http://127.0.0.1:5500/';
 
-if(window.location.href == host + 'addword.html' ){
-    /* ADD NEW WORD SECTION */
 var wordToSave = document.getElementById('addWord');
 var btnSaveWord = document.getElementById('btnSaveWord');
 var wordsList = ['AMOR', 'PASION', 'CORAZON'];
 
 
+
+    /* ADD NEW WORD SECTION */
 btnSaveWord.addEventListener('click', ()=>{
     if(wordsList.indexOf(wordToSave.value) !== -1 && typeof (wordToSave.value != 'string') ){
         alert('Aqui hay un maco')
@@ -19,6 +19,5 @@ btnSaveWord.addEventListener('click', ()=>{
     }
     
 });
-}
 
 /*  GAME SECTION  */
