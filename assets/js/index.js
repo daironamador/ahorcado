da213a359f4c50c31d29b12facc89e3aa0c2dfe0
Dairon Amador <daironamador016@gmail.com>
2022-09-14 12:45:25 -0400

    to Backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,51 @@
+'use strict'
 
-const host = 'http://127.0.0.1:5500/';
 
-var wordToSave = document.getElementById('addWord');
-var btnSaveWord = document.getElementById('btnSaveWord');
+// BOTONES
+const btnStartGame = document.getElementById('startGame');
+const btnToAddWord = document.getElementById('addWord');
+const btnNewGame = document.getElementById('newGame');
+const btnCancelGame = document.getElementById('gameCancel');
+const btnSaveWord = document.getElementById('btnSaveWord');
+
+
+// VARIABLES PARA GUARDAR PALABRAS
 var wordsList = ['AMOR', 'PASION', 'CORAZON'];
+var wordToSave = document.getElementById('addWord');
 
 
 
-    /* ADD NEW WORD SECTION */
+
+// FUNCIONES DE LOS BOTONES
+btnStartGame.addEventListener('click', ()=>{
+    document.getElementById('start-section').style.display ='none';
+    document.getElementById('game-section').style.display = 'contents';
+});
+
+btnToAddWord.addEventListener('click', ()=>{
+    document.getElementById('start-section').style.display ='none';
+    document.getElementById('addword-section').style.display = 'contents';
+});
+
+btnCancelGame.addEventListener('click', ()=>{
+    document.getElementById('start-section').style.display ='contents';
+    document.getElementById('game-section').style.display = 'none';
+});
+
+
+
+
+
+/* ADD NEW WORD SECTION */
 btnSaveWord.addEventListener('click', ()=>{
-    if(wordsList.indexOf(wordToSave.value) !== -1 && typeof (wordToSave.value != 'string') ){
+    if(wordsList.indexOf(wordToSave.value)=== -1 && typeof (wordToSave.value != 'string') ){
         alert('Aqui hay un maco')
     } else{
         wordsList.push(wordToSave.value);
         localStorage.setItem('palabras', JSON.stringify(wordsList));
         wordToSave.value='';
-        location.href='game.html';
+        document.getElementById('game-section').style.display ='contents';
+        document.getElementById('addword-section').style.display = 'none';
     }
     
 });
